9c2fcd71b9c3eafd1a99d8802bceb661aea00581
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-23 23:28:16 +0530

    Add test macros and counters and fix failing test for strlen

--- src/library/strutils.test.asm
+++ src/library/strutils.test.asm
@@ -5,12 +5,24 @@
 ; MACROS
 ; ------
 
+%macro PASS 1
+    PRINT msg_pass                  ; Print "PASS: "
+    PRINT %1                        ; Print the test name
+%endmacro
+
 %macro FAIL 1         
     PRINT msg_fail                  ; Print "FAIL: "
     PRINT %1                        ; Print the test name
     ; PRINT %2                        ; Print the expected value
     ; PRINT %3                        ; Print the actual value
     EXIT EXIT_FAILURE               ; Exit with failure status
+%endmacro
+
+%macro TEST 1
+    jne .%1_failed                  ; If the test fails, jump to the failure label
+    inc r8                          ; Increment the test counter
+    inc r9                          ; Increment the total tests counter
+    PASS test_%1                    ; Print "PASS: " followed by the test name
 %endmacro
 
 ; INITIALIZED DATA
@@ -20,11 +32,14 @@ section .data
     newline db 0xA
     newline_len equ 1
 
+    msg_pass db 0x1b, '[32mPASS: ', 0x1b, '[0m'
+    msg_pass_len equ $ - msg_pass
+
     msg_fail db 0x1b, '[33mFAIL: ', 0x1b, '[0m' 
     msg_fail_len equ $ - msg_fail
     
     test_strlen db 'strlen should return the length of this string', 0xA, 0
-    test_strlen_len equ $ - test_strlen
+    test_strlen_len equ $ - test_strlen - 1 ; Exclude the null terminator from the length
 
 ; MAIN
 ; ----
@@ -38,14 +53,17 @@ _start:
     ; TEST CASES
     ; ----------
 
+    mov r8, 0                      ; Initialize test counter to 0
+    mov r9, 0                      ; Initialize total tests counter to 0
+
     ; ------- TEST: strlen -------
 
     mov rdi, test_strlen
     call strlen
     ; rax now contains the length of the string
-    cmp rax, test_strlen_len  ; Compare the result with the expected length
-    jne .strlen_failed  ; If not equal, jump to the failure label
-    ; If equal, continue with the next test or exit
+    mov r10, rax                    ; Store the result in r10 for comparison
+    cmp r10, test_strlen_len        ; Compare the result with the expected length
+    TEST strlen
 
     ; ------- ALL TESTS PASSED -------
 
