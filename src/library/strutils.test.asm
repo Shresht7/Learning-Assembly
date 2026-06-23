fdec56da35ceab68a7df5ca0760102f4c7f9f916
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-24 00:27:37 +0530

    Add `strcmp` function  for string comparison

--- src/library/strutils.test.asm
+++ src/library/strutils.test.asm
@@ -41,6 +41,27 @@ section .data
     test_strlen db 'strlen should return the length of this string', 0xA, 0
     test_strlen_len equ $ - test_strlen - 1 ; Exclude the null terminator from the length
 
+    test_strcmp_equal db 'strcmp should return 0 for equal strings', 0xA, 0
+    test_strcmp_equal_len equ $ - test_strcmp_equal - 1 
+    test_strcmp_equal_1 db 'these are the same', 0
+    test_strcmp_equal_1_len equ $ - test_strcmp_equal_1
+    test_strcmp_equal_2 db 'these are the same', 0
+    test_strcmp_equal_2_len equ $ - test_strcmp_equal_2
+
+    test_strcmp_less db 'strcmp should return a negative value for str1 < str2', 0xA, 0
+    test_strcmp_less_len equ $ - test_strcmp_less - 1
+    test_strcmp_less_1 db 'Alice', 0
+    test_strcmp_less_1_len equ $ - test_strcmp_less_1
+    test_strcmp_less_2 db 'Bob', 0
+    test_strcmp_less_2_len equ $ - test_strcmp_less_2
+
+    test_strcmp_greater db 'strcmp should return a positive value for str1 > str2', 0xA, 0
+    test_strcmp_greater_len equ $ - test_strcmp_greater - 1
+    test_strcmp_greater_1 db 'Bob', 0
+    test_strcmp_greater_1_len equ $ - test_strcmp_greater_1
+    test_strcmp_greater_2 db 'Alice', 0
+    test_strcmp_greater_2_len equ $ - test_strcmp_greater_2
+
 ; MAIN
 ; ----
 
@@ -65,6 +86,34 @@ _start:
     cmp r10, test_strlen_len        ; Compare the result with the expected length
     TEST strlen
 
+    ; ------- TEST: strcmp -------
+
+    ; Test case 1: Equal strings
+    mov rdi, test_strcmp_equal_1
+    mov rsi, test_strcmp_equal_2
+    call strcmp
+    ; rax now contains the result of the comparison
+    cmp rax, 0
+    TEST strcmp_equal
+
+    ; Test case 2: str1 < str2
+    mov rdi, test_strcmp_less_1
+    mov rsi, test_strcmp_less_2
+    call strcmp
+    ; rax now contains the result of the comparison
+    cmp rax, 0
+    jg .strcmp_less_failed
+    PASS test_strcmp_less
+
+    ; Test case 3: str1 > str2
+    mov rdi, test_strcmp_greater_1
+    mov rsi, test_strcmp_greater_2
+    call strcmp
+    ; rax now contains the result of the comparison
+    cmp rax, 0
+    jl .strcmp_greater_failed
+    PASS test_strcmp_greater
+
     ; ------- ALL TESTS PASSED -------
 
     EXIT EXIT_SUCCESS
@@ -75,3 +124,12 @@ _start:
 
 .strlen_failed:
     FAIL test_strlen
+
+.strcmp_equal_failed:
+    FAIL test_strcmp_equal
+
+.strcmp_less_failed:
+    FAIL test_strcmp_less
+
+.strcmp_greater_failed:
+    FAIL test_strcmp_greater
