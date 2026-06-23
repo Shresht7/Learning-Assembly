06d7c90f30eb3e36446e4ae19be14fd73833fa03
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-23 23:33:08 +0530

    Add `print_str` function to output null-terminated strings to stdout

--- src/library/strutils.asm
+++ src/library/strutils.asm
@@ -22,6 +22,25 @@ section .text
     .strlen_done:
         ret                             ; Return from the function, with rax containing the length of the string
 
+    ; print_str(rdi: *str)
+    ; Prints a null-terminated string to stdout.
+    ;
+    ; @param rdi: pointer to the null-terminated string
+    ; @return: nothing
+    print_str:
+        push rdi                        ; Save the pointer to the string on the stack for safekeeping while we calculate its length
+
+        call strlen                     ; Call the strlen function to get the length of the string (in rax)
+        mov rdx, rax                    ; Move the length of the string into rdx (the count for the write syscall)
+
+        pop rsi                         ; Restore the pointer to the string from the stack into rsi (the buffer for the write syscall)
+
+        mov rax, SYSCALL_WRITE          ; Set rax to the syscall number for write
+        mov rdi, STDOUT                 ; Set rdi to the file descriptor for stdout
+        syscall                         ; Execute the write syscall to print the string to stdout
+
+        ret
+
 
 
 %endif; STRUTILS_ASM
