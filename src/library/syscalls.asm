ef6a146a9594d7e8165e5a3114452b230db5a772
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-24 19:25:25 +0530

    Enhance syscall macros with detailed documentation

--- src/library/syscalls.asm
+++ src/library/syscalls.asm
@@ -19,34 +19,58 @@ STDERR          equ 2
 EXIT_SUCCESS    equ 0
 EXIT_FAILURE    equ 1
 
-; WRITE macro to write a message to a file
+; WRITE <file-descriptor>, <*buffer>, <count>
+;
+; Writes data from a buffer to a file descriptor (e.g., stdout, stderr).
+;
+; @param file-descriptor: The file descriptor to write to (e.g., STDOUT, STDERR)
+; @param *buffer: Pointer to the buffer containing the data to write
+; @param count: The number of bytes to write from the buffer
+; @returns rax: The number of bytes written or a negative value if an error occurred
 %macro WRITE 3
     mov rax, SYSCALL_WRITE          ; syscall: write
-    mov rdi, %1                     ; file descriptor: %1 (e.g., STDOUT)
-    mov rsi, %2                     ; buffer: pointer to the message
-    mov rdx, %3                     ; count: length of the message
+    mov rdi, %1                     ; file descriptor: (e.g., STDOUT, STDERR)
+    mov rsi, %2                     ; buffer: pointer to the string to write
+    mov rdx, %3                     ; count: length of the bytes to write
     syscall                         ; execute syscall
 %endmacro
 
-; READ macro to read input from a file
+; READ <file-descriptor>, <*buffer>, <count>
+;
+; Reads data from a file descriptor (e.g., stdin) into a buffer.
+;
+; @param file-descriptor: The file descriptor to read from (e.g., STDIN)
+; @param *buffer: Pointer to the buffer where the read data will be stored
+; @param count: The maximum number of bytes to read into the buffer
+; @returns rax: The number of bytes read or a negative value if an error occurred
 %macro READ 3
     mov rax, SYSCALL_READ           ; syscall: read
-    mov rdi, %1                     ; file descriptor: %1 (e.g., STDIN)
-    mov rsi, %2                     ; buffer: pointer to the buffer
-    mov rdx, %3                     ; count: number of bytes to read
+    mov rdi, %1                     ; file descriptor: (e.g., STDIN)
+    mov rsi, %2                     ; buffer: pointer to the buffer to store the read data
+    mov rdx, %3                     ; count: number of bytes to read from the file descriptor
     syscall                         ; execute syscall
 %endmacro
 
-; PRINT macro to print a message to stdout
+; PRINT <defined-string>
+;
+; Prints the defined string to STDOUT
+;
+; @param *str: The defined string to print to stdout. (must also have ..._len defined alongside it)
+; @returns rax: The number of bytes written or a negative value if an error occurred
 %macro PRINT 1
     mov rax, SYSCALL_WRITE          ; syscall: write
     mov rdi, STDOUT                 ; file descriptor: stdout
-    mov rsi, %1                     ; buffer: pointer to the message
-    mov rdx, %1_len                 ; count: length of the message
+    mov rsi, %1                     ; buffer: pointer to the string to write
+    mov rdx, %1_len                 ; count: length of the string
     syscall                         ; execute syscall
 %endmacro
 
-; ERROR macro to print an error message to stderr
+; ERROR <defined-string>
+;
+; Prints the defined string to STDERR
+;
+; @param *str: The defined string to print to stderr. (must also have ..._len defined alongside it)
+; @returns rax: The number of bytes written or a negative value if an error occurred
 %macro ERROR 1
     mov rax, SYSCALL_WRITE          ; syscall: write
     mov rdi, STDERR                 ; file descriptor: stderr
@@ -55,14 +79,26 @@ EXIT_FAILURE    equ 1
     syscall                         ; execute syscall
 %endmacro
 
-; EXIT macro to exit the program with a given status code
+; EXIT <exit-code>
+;
+; Exits the program with the specified exit code.
+;
+; @param exit-code: The exit code to return to the operating system (e.g., EXIT_SUCCESS, EXIT_FAILURE, 0, 1, 255, etc.)
+; @returns: This macro does not return; it terminates the program.
 %macro EXIT 1
     mov rax, SYSCALL_EXIT           ; syscall: exit
     mov rdi, %1                     ; status: exit code
     syscall                         ; execute
 %endmacro
 
-; DEFINE_STR macro to define a string in the data section
+; DEFINE_STR <label>, <string>
+;
+; Defines a string in the data section with a corresponding length label.
+; @note This must be used in the data section of the assembly code, not in the text section.
+;
+; @param label: The label for the string
+; @param string: The string to define
+; @returns: This macro does not return a value; it defines a string and its length in the data section.
 %macro DEFINE_STR 2+
     %1: db %2
     %1_len: equ $ - %1
